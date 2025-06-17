724ffd5d6e7d41b7679c29f3e327009382fb818b
Sebastian <spratto@teco.com.ar>
2025-06-17 16:31:22 -0300

    add viewModel injection

--- app/src/main/java/ar/com/mychallenge/di/AppModule.kt
+++ app/src/main/java/ar/com/mychallenge/di/AppModule.kt
@@ -3,9 +3,12 @@ package ar.com.mychallenge.di
 import ar.com.mychallenge.data.remote.CityApiService
 import ar.com.mychallenge.data.repository.CityRepository
 import ar.com.mychallenge.data.repository.CityRepositoryImpl
+import ar.com.mychallenge.presentation.city.viewmodel.CityViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val appModule = module {
     single { CityApiService() }
-    single<CityRepository> { CityRepositoryImpl(apiService = get()) }
+    single<CityRepository> { CityRepositoryImpl(get()) }
+    viewModel { CityViewModel(get()) }
 }
