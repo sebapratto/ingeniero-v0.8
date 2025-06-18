e4d030df5b7678559e040352b755d1d803ff6bdb
Sebastian <spratto@teco.com.ar>
2025-06-18 10:26:08 -0300

    set new values config

--- app/src/main/java/ar/com/mychallenge/ui/theme/Color.kt
+++ app/src/main/java/ar/com/mychallenge/ui/theme/Color.kt
@@ -9,3 +9,7 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val OrangeStrong = Color(0xFFD84242)
+val OrangeGrey = Color(0xffb85959)
+val Orange = Color(0xfff46262)
